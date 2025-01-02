0462b4d289aaf9e3d12ee66a1e4abfe5791686b6
David Caballero <davidcame124@gmail.com>
2025-01-02 10:52:43 -0500

    end calendar

--- day/models.py
+++ day/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Day(models.Model):
+    ejercicio = models.BooleanField(default=False)
+    c = models.BooleanField(default=False)
+    python = models.BooleanField(default=False)
+    ingles = models.BooleanField(default=False)
+    libro = models.BooleanField(default=False)
+    dia = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        dia_formateado = self.dia.strftime('%d-%m-%Y') 
+        return str(self.id) + "- " + dia_formateado
